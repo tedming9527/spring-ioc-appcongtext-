8e91861fb090ef330e7cd570abb2c91ba549d0de
tedming9527 <tedming9527@gmail.com>
2025-01-08 17:47:39 +0800

    doc: java spring

---
+++ src/main/java/com/itranswarp/learnjava/service/MailService.java
@@ -0,0 +1,25 @@
+package com.itranswarp.learnjava.service;
+
+import org.springframework.stereotype.Component;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class MailService {
+  private ZoneId zoneId = ZoneId.systemDefault();
+
+  public void setZoneId(ZoneId zoneId) {
+    this.zoneId = zoneId;
+  }
+  public String getTime() {
+    return ZonedDateTime.now(this.zoneId).format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
+  }
+  public void sendLoginMail(User user) {
+    System.err.printf("Hi, %s! You are logged in at %s%n", user.getName(), getTime());
+  }
+  public void sendRegistrationMail(User user) {
+    System.err.println(String.format("Welcome, %s", user.getName()));
+  }
+}
---
+++ src/main/java/com/itranswarp/learnjava/service/UserService.java
@@ -0,0 +1,46 @@
+package com.itranswarp.learnjava.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserService {
+  @Autowired
+  private MailService mailService;
+
+  public void setMailService(MailService mailService) {
+    this.mailService = mailService;
+  }
+  private List<User> users = new ArrayList<>(List.of(
+      new User(1, "bob@example.com", "password", "Bob"),
+      new User(2, "alice@example.com", "password", "Alice"),
+      new User(3, "tom@example.com", "password", "Tom")
+  ));
+
+  public User login(String email, String password) {
+    for (User user: users) {
+      if (user.getEmail().equalsIgnoreCase(email) && user.getPassword().equals(password)) {
+        mailService.sendLoginMail(user);
+        return user;
+      }
+    }
+    throw new RuntimeException("login failed.");
+  }
+  public User getUser(long id) {
+    return this.users.stream().filter(user -> user.getId() == id).findFirst().orElseThrow();
+  }
+  public User register(String email, String password, String name) {
+    users.forEach(user -> {
+      if (user.getEmail().equalsIgnoreCase(email)) {
+        throw new RuntimeException("email exist.");
+      }
+    });
+    User user = new User(users.stream().mapToLong(User::getId).max().getAsLong() + 1, email, password, name);
+    users.add(user);
+    mailService.sendRegistrationMail(user);
+    return user;
+  }
+}
