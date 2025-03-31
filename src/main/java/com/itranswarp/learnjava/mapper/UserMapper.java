4018a69006ef77c409bbaf5d14f2ce602153e70b
tedming9527 <tedming9527@gmail.com>
2025-03-31 16:57:53 +0800

    fix(专题页): 便民商品样式优化

---
+++ src/main/java/com/itranswarp/learnjava/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.itranswarp.learnjava.model;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface UserMapper {
+  @Select("select * from users where id = #{id}")
+  User getById(@Param("id") long id);
+
+  @Select("select *  from users limit #{offset}, #{maxResults}")
+  List<User> getAll(@Param("offset") int offset, @Param("maxResults") int maxResults);
+
+  @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+  @Insert("insert into users (email, password, name createAt) values (#{user.email}, #{user.password}, #{user.password}, #{user.createAt})")
+  void insert(@Param("user") User user);
+
+}
