74aa8930879b9a743e28ef25a0a8f2acf12b0bca
tedming9527 <tedming9527@gmail.com>
2025-04-11 14:28:10 +0800

    fix(报事): 页面跳转清楚客户信息控制

--- databasetest/src/main/java/com/itranswarp/learnjava/service/AppConfig.java
+++ databasetest/src/main/java/com/itranswarp/learnjava/service/AppConfig.java
@@ -9,11 +9,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan
+@EnableTransactionManagement
 @PropertySource("jdbc.properties")
 public class AppConfig {
   @Value("${jdbc.url}")
@@ -40,6 +44,10 @@ public class AppConfig {
   @Bean
   JdbcTemplate createJdbcTemplate(@Autowired DataSource dataSource) {
     return new JdbcTemplate(dataSource);
+  }
+  @Bean
+  PlatformTransactionManager createTxManager(@Autowired DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
   }
 
 }
--- databasetest/src/main/java/com/itranswarp/learnjava/service/UserService.java
+++ databasetest/src/main/java/com/itranswarp/learnjava/service/UserService.java
@@ -1,13 +1,27 @@
 package com.itranswarp.learnjava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+import java.lang.management.PlatformManagedObject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
 
 @Component
+@Transactional
 public class UserService {
   @Autowired
   JdbcTemplate jdbcTemplate;
@@ -40,9 +54,44 @@ public class UserService {
               rs.getString("email"),
               rs.getString("password"),
               rs.getString("name")
-          )
+          );
         }
       }
-    })
+    });
+  }
+  public User getUserByEmail(String email) {
+    return jdbcTemplate.queryForObject("Select * from users where email = ?", (ResultSet rs, int rowNum) -> {
+      return new User(
+          rs.getLong("id"),
+          rs.getString("email"),
+          rs.getString("password"),
+          rs.getString("name")
+      );
+    }, email);
+  }
+  public List<User> getUsers(int pageIndex) {
+    int limit = 100;
+    int offset = limit * (pageIndex - 1);
+    return jdbcTemplate.query("Select * from users limit ? offset ?", new BeanPropertyRowMapper<User>(User.class), limit, offset);
+  }
+  public void updateUser(User user) {
+    if (1 != jdbcTemplate.update("update users set name = ? where id = ?", user.getName(), user.getId())) {
+      throw new RuntimeException("User not found by id");
+    }
+  }
+
+  @Transactional
+  public User register(String email, String password, String name) {
+    KeyHolder holder = new GeneratedKeyHolder();
+    if (1 != jdbcTemplate.update(conn -> {
+      var ps = conn.prepareStatement("Insert into users(email, password, name) values(?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+      ps.setObject(1, email);
+      ps.setObject(2, password);
+      ps.setObject(3, name);
+      return  ps;
+    })) {
+      throw new RuntimeException("Insert failed.");
+    }
+    return  new User(holder.getKey().longValue(), email, password, name);
   }
 }
